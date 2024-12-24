c9897f711cd763996661926ef8230cd14b1b84db
Axel Delille <Axel.delille31@gmail.com>
2024-12-24 01:39:34 +0100

    feat: Grammar v1 for dali

---
+++ langium/Dali-Movie/src/language/dali-movie-validator.ts
@@ -0,0 +1,31 @@
+import type { ValidationAcceptor, ValidationChecks } from 'langium';
+import type { DaliMovieAstType, Person } from './generated/ast.js';
+import type { DaliMovieServices } from './dali-movie-module.js';
+
+/**
+ * Register custom validation checks.
+ */
+export function registerValidationChecks(services: DaliMovieServices) {
+    const registry = services.validation.ValidationRegistry;
+    const validator = services.validation.DaliMovieValidator;
+    const checks: ValidationChecks<DaliMovieAstType> = {
+        Person: validator.checkPersonStartsWithCapital
+    };
+    registry.register(checks, validator);
+}
+
+/**
+ * Implementation of custom validations.
+ */
+export class DaliMovieValidator {
+
+    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
+        if (person.name) {
+            const firstChar = person.name.substring(0, 1);
+            if (firstChar.toUpperCase() !== firstChar) {
+                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+            }
+        }
+    }
+
+}
---
+++ langium/Dali-Movie/src/setupExtended.ts
@@ -0,0 +1,58 @@
+import { MonacoEditorLanguageClientWrapper, UserConfig } from 'monaco-editor-wrapper';
+import { configureWorker, defineUserServices } from './setupCommon.js';
+
+export const setupConfigExtended = (): UserConfig => {
+    const extensionFilesOrContents = new Map();
+    extensionFilesOrContents.set('/language-configuration.json', new URL('../language-configuration.json', import.meta.url));
+    extensionFilesOrContents.set('/dali-movie-grammar.json', new URL('../syntaxes/dali-movie.tmLanguage.json', import.meta.url));
+
+    return {
+        wrapperConfig: {
+            serviceConfig: defineUserServices(),
+            editorAppConfig: {
+                $type: 'extended',
+                languageId: 'dali-movie',
+                code: `// Dali Movie is running in the web!`,
+                useDiffEditor: false,
+                extensions: [{
+                    config: {
+                        name: 'dali-movie-web',
+                        publisher: 'generator-langium',
+                        version: '1.0.0',
+                        engines: {
+                            vscode: '*'
+                        },
+                        contributes: {
+                            languages: [{
+                                id: 'dali-movie',
+                                extensions: [
+                                    '.dali-movie'
+                                ],
+                                configuration: './language-configuration.json'
+                            }],
+                            grammars: [{
+                                language: 'dali-movie',
+                                scopeName: 'source.dali-movie',
+                                path: './dali-movie-grammar.json'
+                            }]
+                        }
+                    },
+                    filesOrContents: extensionFilesOrContents,
+                }],                
+                userConfiguration: {
+                    json: JSON.stringify({
+                        'workbench.colorTheme': 'Default Dark Modern',
+                        'editor.semanticHighlighting.enabled': true
+                    })
+                }
+            }
+        },
+        languageClientConfig: configureWorker()
+    };
+};
+
+export const executeExtended = async (htmlElement: HTMLElement) => {
+    const userConfig = setupConfigExtended();
+    const wrapper = new MonacoEditorLanguageClientWrapper();
+    await wrapper.initAndStart(userConfig, htmlElement);
+};
